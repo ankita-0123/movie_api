bda78a4ddc2d16dbe09b415295981c98fe20e1ff
ankita-0123 <pragyanparamita1449@gmail.com>
2023-03-17 20:29:08 +0530

    commit id

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import { useState } from 'react';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  function fetchMoviesHandler() {
-    fetch('https://swapi.dev/api/films/').then(response=>{
-      return response.json();
-    }).then(data=>{
+  async function fetchMoviesHandler() {
+    const response = await fetch('https://swapi.dev/api/films/');
+    const data = await response.json();
+    
       const transformedMovies = data.results.map(movieData=> {
         return{
           id:movieData.episode_id,
@@ -20,7 +20,7 @@ function App() {
 
       });
       setMovies(transformedMovies);
-    });
+   
   }
   return (
     <Fragment>
