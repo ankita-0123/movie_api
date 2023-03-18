00d27d26adec1d1bce1fb6a587ca1406a00cceba
ankita-0123 <pragyanparamita1449@gmail.com>
2023-03-18 15:45:56 +0530

    commit id

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{Fragment} from 'react';
+import React,{Fragment, useEffect, useCallback} from 'react';
 import MovieList from './components/MovieList';
 import './App.css';
 import { useState } from 'react';
@@ -7,11 +7,11 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async()=> {
     setIsLoading(true);
     setError(null);
     try{
-      const response = await fetch('https://swapi.dev/api/film/');
+      const response = await fetch('https://swapi.dev/api/films/');
       if(!response.ok) {
         throw new Error('Something went wrong ....Retrying');
       }
@@ -33,7 +33,10 @@ function App() {
       setError(error.message)
     }
     setIsLoading(false); 
-  }
+  },[]);
+  useEffect(()=> {
+    fetchMoviesHandler();
+  },[fetchMoviesHandler]);
   let content = <p>Found no movie.</p>;
   if(movies.length>0){
     content=<MovieList movies={movies}/>
